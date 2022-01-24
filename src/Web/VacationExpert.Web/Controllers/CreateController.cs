3285082eaff13597b4c235f87010c81c3c3f6802
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2022-01-24 17:07:08 +0200

    Create page styling

---
+++ src/Web/VacationExpert.Web/Controllers/CreateController.cs
@@ -0,0 +1,12 @@
+﻿namespace VacationExpert.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CreateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
